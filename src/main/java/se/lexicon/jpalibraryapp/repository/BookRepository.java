380b655ac249e5729127671e5a30e5739e1c2437
Iffat-Sudo <zabin.ifa@gmail.com>
2026-01-22 10:40:42 +0100

    Added all changes

---
+++ src/main/java/se/lexicon/jpalibraryapp/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package se.lexicon.jpalibraryapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.lexicon.jpalibraryapp.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
